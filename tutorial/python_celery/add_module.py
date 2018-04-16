d0873a30e8817267cc4b729db26052c86cb7ba4f
SabraO <sabraosn@gmail.com>
2018-04-16 01:20:11 -0400

    remove previous files

--- tutorial/python_celery/add_module.py
+++
@@ -1,9 +0,0 @@
-from celery import Celery
-    
-app = Celery('add_module')
-app.config_from_object('celeryconfig')
-
-    
-@app.task(name='add_module.tasks.add')
-def add_two_numbers(x, y):
-    return x + y
--- tutorial/python_celery/celeryconfig.py
+++
@@ -1,9 +0,0 @@
-broker_url = 'redis://localhost:6379/0' # for redis
-#broker_url = 'pyamqp://' # for rabbitmq 
-result_backend = 'rpc://'
-result_persistent = True
-task_routes = {
-    'add_module.tasks.add': {'queue':'add'},
-    'mat_mul_module.tasks.mat_mul': {'queue':'mat_mul'},
-    'factorial_module.tasks.factorial': {'queue':'factorial'}
-}
